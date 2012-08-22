b3bc8fb4a5f2e3b7288130abfef8220a38c8afb9
Hannah Copperman <hcopperm@Hannah-Coppermans-MacBook-Air.local>
2012-08-22 14:05:25 -0700

    made colors random and added clear button

--- loopy.js
+++ loopy.js
@@ -2,22 +2,22 @@ function arcBusiness(clickEvent) {
   this.clickEvent = clickEvent;
   this.degrees = 360;
   this.radians = (Math.PI/180)*this.degrees;
+  this.ctx = document.getElementById('tutorial').getContext('2d');
 
   this.stepColor = function(startPoint) {
     return Math.floor(255-42.5*(startPoint % 3));
   } 
   
-  this.color = "rgb(" + this.stepColor(this.clickEvent.clientY) + ", " + this.stepColor(this.clickEvent.clientX) + ", 0)";  
+  this.color = "rgb(" + this.stepColor(this.clickEvent.clientY) + ", " + this.stepColor(this.clickEvent.clientX) +", " + this.stepColor(Math.random() * 90) + ")";  
 
 
   this.drawArcs = function() {
-    for (var x = 0; x < 50; x++) {
+    for (var x = 2; x < 50; x++) {
       this.delayArc(x*20, 1);
     }
   }
 
   this.eraseArcs = function() {
-    this.color = "red";
     for (var x = 50; x > 0; x--) {
       this.delayArc(x*20, -1);
     }
@@ -25,28 +25,19 @@ function arcBusiness(clickEvent) {
 
   this.delayArc = function(start, delay) {
     var arc = this;
+    arc.makeArc(20);
     setTimeout(function() {
      arc.makeArc(start); 
-    }, (1000 + (start*delay)));
+    }, (start*delay));
   }
 
   this.makeArc = function(start) {
-    var ctx = document.getElementById('tutorial').getContext('2d');
-    ctx.beginPath();
-    ctx.arc((this.clickEvent.clientX), this.clickEvent.clientY, start/2, 0, this.radians, true);
-    ctx.closePath();
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = this.color;
-    ctx.stroke();
+    this.ctx.beginPath();
+    this.ctx.arc((this.clickEvent.clientX), this.clickEvent.clientY, start/2, 0, this.radians, true);
+    this.ctx.closePath();
+    this.ctx.lineWidth = 5;
+    this.ctx.strokeStyle = this.color;
+    this.ctx.stroke();
   }
-
-  this.drawAndEraseArcs = function() {
-    var arc = this;
-    setTimeout(function() {
-      arc.drawArcs();
-     // setTimeout(function () {arc.eraseArcs()}, 2400);
-    }, 3000);
-  }
-
 }
      
