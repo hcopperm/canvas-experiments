1dcac9d5d193127c2024751605faf1eeba0f951b
Hannah Copperman <hcopperm@Hannah-Coppermans-MacBook-Air.local>
2012-08-24 15:09:05 -0700

    added squares

--- loopy.js
+++ loopy.js
@@ -1,14 +1,55 @@
-function arcBusiness(clickEvent) {
-  this.clickEvent = clickEvent;
-  this.degrees = 360;
-  this.radians = (Math.PI/180)*this.degrees;
-  this.ctx = document.getElementById('tutorial').getContext('2d');
+function DrawBasics(clickEvent) {
+
+  this.canvas = document.getElementById("tutorial");
+  this.ctx = this.canvas.getContext('2d'); 
+
 
   this.stepColor = function(startPoint) {
     return Math.floor(255-42.5*(startPoint % 3));
-  } 
+  }
   
-  this.color = "rgb(" + this.stepColor(this.clickEvent.clientY) + ", " + this.stepColor(this.clickEvent.clientX) +", " + this.stepColor(Math.random() * 90) + ")";  
+
+
+  this.relMouseCoords = function(){
+      var totalOffsetX = 0;
+      var totalOffsetY = 0;
+      var canvasX = 0;
+      var canvasY = 0;
+      var currentElement = this.canvas;
+  
+      do{
+          totalOffsetX += currentElement.offsetLeft;
+          totalOffsetY += currentElement.offsetTop;
+      }
+      while(currentElement = currentElement.offsetParent)
+  
+      canvasX = clickEvent.pageX - totalOffsetX;
+      canvasY = clickEvent.pageY - totalOffsetY;
+  
+      this.mouseCoords =  {x:canvasX, y:canvasY}
+  }
+  this.relMouseCoords();
+  
+  this.color = "rgb(" + this.stepColor(this.mouseCoords.y) + ", " + this.stepColor(this.mouseCoords.x) +", " + this.stepColor(Math.random() * 90) + ")";  
+}
+
+
+
+function SquareBusiness(clickEvent) {
+  this.drawing = new DrawBasics(clickEvent);
+  this.drawSquare = function() {
+    this.drawing.ctx.fillStyle = this.drawing.color;
+    this.drawing.ctx.fillRect(this.drawing.mouseCoords.x, this.drawing.mouseCoords.y, 20, 20);    
+  }
+
+}
+
+
+function ArcBusiness(clickEvent) {
+  this.drawing = new DrawBasics(clickEvent);
+  this.degrees = 360;
+  this.radians = (Math.PI/180)*this.degrees;
+
 
 
   this.drawArcs = function() {
@@ -32,12 +73,12 @@ function arcBusiness(clickEvent) {
   }
 
   this.makeArc = function(start) {
-    this.ctx.beginPath();
-    this.ctx.arc((this.clickEvent.clientX), this.clickEvent.clientY, start/2, 0, this.radians, true);
-    this.ctx.closePath();
-    this.ctx.lineWidth = 5;
-    this.ctx.strokeStyle = this.color;
-    this.ctx.stroke();
+    this.drawing.ctx.beginPath();
+    this.drawing.ctx.arc(this.drawing.mouseCoords.x, this.drawing.mouseCoords.y, start/2, 0, this.radians, true);
+    this.drawing.ctx.closePath();
+    this.drawing.ctx.lineWidth = 5;
+    this.drawing.ctx.strokeStyle = this.drawing.color;
+    this.drawing.ctx.stroke();
   }
 }
      
