1f45e7d3f20f64c7be81189446d4a19b379373d1
Hannah Copperman <hcopperm@Hannah-Coppermans-MacBook-Air.local>
2012-09-17 15:42:59 -0700

    added skulls

--- loopy.js
+++ loopy.js
@@ -2,12 +2,13 @@ function DrawBasics(clickEvent) {
 
   this.canvas = document.getElementById("tutorial");
   this.ctx = this.canvas.getContext('2d'); 
+  this.degrees = 360;
+  this.radians = (Math.PI/180)*this.degrees;
 
   this.stepColor = function(startPoint) {
     return Math.floor(255-42.5*(startPoint % 3));
   }
   
-
 
   this.relMouseCoords = function(){
       var totalOffsetX = 0;
@@ -30,10 +31,22 @@ function DrawBasics(clickEvent) {
   this.relMouseCoords();
   
   this.color = "rgb(" + this.stepColor(this.mouseCoords.y) + ", " + this.stepColor(this.mouseCoords.x) +", " + this.stepColor(Math.random() * 90) + ")";  
+
+  this.makeArc = function(start) {
+
+    this.ctx.beginPath();
+    this.ctx.arc(this.mouseCoords.x, this.mouseCoords.y, start/2, 0, this.radians, true);
+    this.ctx.closePath();
+    this.ctx.lineWidth = 2;
+    this.ctx.strokeStyle = this.color;
+    this.ctx.stroke();
+    /*this.ctx.fillStyle = this.color;
+    this.ctx.fill();*/
+  }
 }
 
 
-function HeartBusiness(clickEvent) {
+function Heart(clickEvent) {
   this.drawing = new DrawBasics(clickEvent);
   this.x = this.drawing.mouseCoords.x;
   this.y = this.drawing.mouseCoords.y;
@@ -47,7 +60,7 @@ function HeartBusiness(clickEvent) {
     ctx.bezierCurveTo(this.x + 35, this.y + 62, this.x + 55, this.y + 40, this.x + 55, this.y + 22.5);
     ctx.bezierCurveTo(this.x + 55, this.y + 22.5, this.x + 55, this.y - 15, this.x + 25, this.y - 15);
     ctx.bezierCurveTo(this.x + 10, this.y - 15, this.x, this.y - 3, this.x, this.y);
-    ctx.fillStyle = "pink";
+    ctx.fillStyle = "#F52C7D";
     ctx.fill();
   }
 }
@@ -62,13 +75,109 @@ function SquareBusiness(clickEvent) {
 
 }
 
+function Skull(clickEvent) {
+  this.drawing = new DrawBasics(clickEvent);
+  this.drawing.color = "black";
+  this.x = this.drawing.mouseCoords.x;
+  this.y = this.drawing.mouseCoords.y;
+  this.makeSkull = function(start) {
+    this.makeCranium(65);
+    this.makeJaw();
+    this.makeMouth();
+    this.makeEyes();
+    this.makeNose();
+    this.makeTeeth();
+  }
+  this.makeCranium = function(start) {
+    var ctx = this.drawing.ctx;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, start/2, (Math.PI/180) * 48, (Math.PI/180) * 132, true);
+    //ctx.closePath();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = this.color;
+    ctx.stroke();
+    /*this.ctx.fillStyle = this.color;
+    this.ctx.fill();*/
+  }
+
+  this.makeJaw = function() {
+    var ctx = this.drawing.ctx;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y+30, 23, (Math.PI/180)*195, (Math.PI/180)*345, true);
+    //ctx.closePath();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = this.drawing.color;
+   // ctx.fillStyle = this.drawing.color;
+   // ctx.fill();
+    ctx.stroke();
+  }
+
+  this.makeEyes = function() {
+    this.makeEye(12);
+    this.makeEye(-12);
+  }
+
+  this.makeEye = function(place) {
+    var ctx = this.drawing.ctx;
+    ctx.beginPath();
+    ctx.arc(this.x - place, this.y + 9.5, 10, 0, this.drawing.radians, true);
+    ctx.closePath();
+    ctx.stroke();
+
+    //ctx.fillStyle = "white";
+    //ctx.fill();
+  }
+
+  this.makeNose = function() {
+    var ctx = this.drawing.ctx;
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y + 19);
+    ctx.lineTo(this.x - 4, this.y + 29);
+    ctx.lineTo(this.x + 4, this.y + 29);
+    ctx.closePath();
+    ctx.stroke();
+  
+  }
+
+  this.makeMouth = function() {
+    var ctx = this.drawing.ctx;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y - 126, 170, (Math.PI/180)*96, (Math.PI/180)*83, true);
+    ctx.lineWidth = 1.75;
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(this.x, this.y - 134, 170, (Math.PI/180)*98, (Math.PI/180)*82, true);
+    ctx.stroke();
+  }
+
+  this.makeTeeth = function() {
+    var ctx = this.drawing.ctx;
+    var z = -20;
+    while (z <= 20) {
+      ctx.beginPath();
+      ctx.moveTo(this.x + z, this.y+36);
+      ctx.lineTo(this.x + z, this.y+43);
+     // ctx.strokeStyle = "white";
+      ctx.stroke();
+      z += 5;
+    }
+  }
+
+
+}
 
 function ArcBusiness(clickEvent) {
   this.drawing = new DrawBasics(clickEvent);
-  this.degrees = 360;
-  this.radians = (Math.PI/180)*this.degrees;
-
-
+  /*
+  this.makeArc = function(start) {
+    this.drawing.ctx.beginPath();
+    this.drawing.ctx.arc(this.drawing.mouseCoords.x, this.drawing.mouseCoords.y, start/2, 0, this.radians, true);
+    this.drawing.ctx.closePath();
+    this.drawing.ctx.lineWidth = 5;
+    this.drawing.ctx.strokeStyle = this.drawing.color;
+    this.drawing.ctx.stroke();
+  }
+*/
 
   this.drawArcs = function() {
     for (var x = 2; x < 50; x++) {
@@ -84,19 +193,12 @@ function ArcBusiness(clickEvent) {
 
   this.delayArc = function(start, delay) {
     var arc = this;
-    arc.makeArc(20);
+    arc.drawing.makeArc(20);
     setTimeout(function() {
-     arc.makeArc(start); 
+     arc.drawing.makeArc(start); 
     }, (start*delay));
   }
 
-  this.makeArc = function(start) {
-    this.drawing.ctx.beginPath();
-    this.drawing.ctx.arc(this.drawing.mouseCoords.x, this.drawing.mouseCoords.y, start/2, 0, this.radians, true);
-    this.drawing.ctx.closePath();
-    this.drawing.ctx.lineWidth = 5;
-    this.drawing.ctx.strokeStyle = this.drawing.color;
-    this.drawing.ctx.stroke();
-  }
+
 }
      
